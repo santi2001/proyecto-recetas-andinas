4c6042cbb13e315f5c238f19d26ac76b28db1c85
santiago churquina <santiago.churquina@pinard.ar>
2022-09-28 17:01:13 -0300

    Arreglos en package.json

--- src/pages/HomePage/components/SearchWidget/SearchWidget.style.js
+++ src/pages/HomePage/components/SearchWidget/SearchWidget.style.js
@@ -48,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
     '& fieldset': {
       border: 'none'
     },
-    [theme.breakpoints.down('lg')]: {
+    [theme.breakpoints.down('md')]: {
       marginTop: theme.spacing(3)
     }
   },
